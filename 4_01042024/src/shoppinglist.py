e5185491b9e5d7d9a97c37418cb6fa28e7d67fce
João Cardoso <136370136+JoaoCardosoDev@users.noreply.github.com>
2024-04-02 18:22:50 +0100

    Fix: MO change and better UX

--- 4_01042024/src/shoppinglist.py
+++ 4_01042024/src/shoppinglist.py
@@ -1,48 +1,59 @@
 import click
 
-
 @click.group()
-@click.pass_context
-def slist(list):
+def slist():
     """
     Welcome to your shopping list
     Please use --help for further instruction.
     """
-    list.ensure_object(dict)
 
 @slist.command()
-@click.pass_context
-def show(list):
+def show():
     """
     Shows your current shopping list.
     """
-    click.echo(f"{list}")
+    try:
+        with open("list.txt", 'r') as list:
+            click.secho("Your Shopping List:", fg='cyan')
+            for line in list:
+                click.echo(f"- {line.strip()}")
+    except FileNotFoundError:
+        click.secho("Your shopping list is empty.", fg='cyan')
 
 @slist.command()
-@click.pass_context
-@click.option('--item', prompt="Add an item to your list\n[Item]: ", help="Input an item to be added to the list.")
-def add(list, item):
+@click.option('--item', prompt=click.style("Add an item to your list\n[Item]", fg='yellow'), help="Input an item to be added to the list.")
+def add(item):
     """
     Adds an item to your list.
     """
-    list.obj.append(item)
-    option = input("Would you like to add more items?\n[Y|N]: ")
-    if option == "Y":
-        add()
-    elif option == "N":
+
+    with open('list.txt', 'a') as list:
+        list.write(f"{item}\n")
+    option = click.prompt(click.style("Would you like to add more items?\n[y|n]", fg='yellow')).lower()
+    if option == "y":
+        add(click.prompt(click.style("Add an item to your list\n[Item]", fg='yellow')))
+    elif option == "n":
         return
     else:
-        click.echo("Invalid input")
+        click.secho("Invalid input", fg='bright_red')
 
-@click.command()
-@click.pass_context
-@click.option('--item', prompt="Which item would you like to remove from the list\n[Item to remove]")
-def remove(list, item):
+@slist.command()
+@click.option('--item', prompt="Which item would you like to remove from the list\n[Item to remove]: ", help="Input an item to be removed from the list.")
+def remove(item):
     """
     Removes an item from your list.
     """
-    list.remove(item)
-    click.echo(f"{item} removed from the list")
-        
+    try:
+        with open("list.txt", 'r') as list:
+            lines = list.readlines()
+        with open("list.txt", "w") as list:
+            for line in lines:
+                if line.strip() != item:
+                    list.write(line)
+        click.secho(f"{item} removed from the list", fg="bright_red")
+    except FileNotFoundError:
+        click.secho("Your list is empty.", fg='cyan')
+    
+
 if __name__ == "__main__":
     slist()
